b6ddb54155bc5d284af97b12251b81149f4c56bd
Martin-J-Larre <martinjlarre@gmail.com>
2022-03-14 00:20:01 -0400

    #3 pseudo api fixed add client test with axios

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -6,7 +6,7 @@ const { getAllProducts, getOneProduct, addProduct, updateProduct,deleteProduct} 
 router.get("/getall", getAllProducts); //Working
 router.get("/getone/:id", getOneProduct); //Working
 router.post("/add", addProduct); //working
-router.put("update/:id", updateProduct);
-router.delete("delete/:id", deleteProduct);
+router.put("/update/:id", updateProduct);
+router.delete("/delete/:id", deleteProduct);
 
 module.exports = router;
